69ed5d0dabf35ae4a0f5e649f9320c4bd988554f
GitTushar <tusharmahajan1632002@gmail.com>
2024-11-27 20:53:38 +0530

    Update App.js

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,40 +1,11 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-import Hero from "./components/Hero/Hero";
 import "./App.css";
 
 function App() {
-  const searchData = [
-    // Example data structure for testing
-    {
-      title: "Album 1",
-      slug: "album-1",
-      songs: [{ artists: ["Artist 1", "Artist 2"] }],
-    },
-    {
-      title: "Album 2",
-      slug: "album-2",
-      songs: [{ artists: ["Artist 3"] }],
-    },
-  ];
-
   return (
-    <Router>
-      <div className="App">
-        <Navbar searchData={searchData} />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <main>
-                <Hero />
-              </main>
-            }
-          />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Navbar />
+    </div>
   );
 }
 
